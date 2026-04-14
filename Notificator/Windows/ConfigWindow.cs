4ed4385a0d52fb90ca2b8a2191ae509704d9ed5a
D3FVLT <whitenobel0@gmail.com>
2026-04-14 10:37:16 +0300

    Refactor Notificator project settings to use Microsoft.NET.Sdk, update target framework to net8.0-windows8.0, and enhance dependency management with additional references. Update build workflow to streamline release preparation.

--- Notificator/Windows/ConfigWindow.cs
+++ Notificator/Windows/ConfigWindow.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Numerics;
-using Dalamud.Bindings.ImGui;
 using Dalamud.Interface.Windowing;
+using ImGuiNET;
 using Notificator.Services;
 
 namespace Notificator.Windows;
